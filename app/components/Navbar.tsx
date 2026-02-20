856a99e72f52e8df1a1b7c7b2eff65a62c109eab
DevPatel1023 <devp4143@gmail.com>
2026-02-20 11:13:15 +0530

    feat: navbar profile-dropdown add

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,6 +6,14 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaRegMoon } from "react-icons/fa";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Bell, CreditCard, LogOut, User } from "lucide-react";
 
 export default function Navbar() {
   return (
@@ -39,10 +47,63 @@ export default function Navbar() {
         </Button>
 
         <div>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            {/* CLICK TRIGGER */}
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+
+            {/* DROPDOWN */}
+            <DropdownMenuContent
+              align="start"
+              side="bottom"
+              sideOffset={8}
+              alignOffset={0}
+              className="w-60 translate-y-2"
+            >
+              {/* USER INFO */}
+              <div className="flex items-center gap-3 p-2">
+                <Avatar className="h-9 w-9">
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>DP</AvatarFallback>
+                </Avatar>
+
+                <div className="grid">
+                  <span className="font-medium">Dev Patel</span>
+                  <span className="text-sm text-muted-foreground">
+                    hello@devpatel.com
+                  </span>
+                </div>
+              </div>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem className="hover:bg-gray-100 transition delay-150 duration-150">
+                <User className="mr-2 h-4 w-4" />
+                Account
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="hover:bg-gray-100 transition delay-150 duration-150">
+                <CreditCard className="mr-2 h-4 w-4" />
+                Billing
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="hover:bg-gray-100 transition delay-150 duration-150">
+                <Bell className="mr-2 h-4 w-4" />
+                Notifications
+              </DropdownMenuItem>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem className="text-red-600 focus:text-red-600 hover:bg-gray-100 transition delay-150 duration-150">
+                <LogOut className="mr-2 h-4 w-4" />
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
